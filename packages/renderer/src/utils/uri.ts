3886ee79c91043a7cf0194208eac113e9b034dee
pansinm <pansinm@foxmail.com>
2022-09-26 16:25:14 +0800

    refactor: markdown & plaintext 判断移动到utils/uri

--- packages/renderer/src/utils/uri.ts
+++ packages/renderer/src/utils/uri.ts
@@ -1,14 +1,26 @@
-import * as monaco from 'monaco-editor';
-
-export const fsPath2Uri = (fsPath: string) => {
-  return monaco.Uri.file(fsPath.replaceAll('\\', '/')).toString();
-};
-
-export const uri2fsPath = (uri: string) => {
-  return monaco.Uri.parse(uri).fsPath;
-};
-
 export const basename = (uri: string) => {
-  const fsPath = uri2fsPath(uri);
-  return fsPath.split('/').pop() as string;
+  const url = new URL(uri);
+  return decodeURIComponent(url.pathname.split('/').pop() || '');
+};
+
+export const fileType = (uri: string) => {
+  const filename = basename(uri);
+  if (/\.mdx?$/.test(filename)) {
+    return 'markdown';
+  }
+  if (/^\.[^.]+$/.test(filename)) {
+    return 'plaintext';
+  }
+  if (/^\.(jsx?|tsx?|txt|text|css|html)$/.test(filename)) {
+    return 'plaintext';
+  }
+  return 'unknown';
+};
+
+export const isMarkdown = (uri: string) => {
+  return fileType(uri) === 'markdown';
+};
+
+export const isPlaintext = (uri: string) => {
+  return fileType(uri) === 'plaintext';
 };
