b8122879093de9e029f73947da9926238f608773
pansinm <pansinm@foxmail.com>
2022-09-26 16:25:14 +0800

    refactor: markdown & plaintext 判断移动到utils/uri

--- packages/renderer/src/main/containers/FileBrowser/index.tsx
+++ packages/renderer/src/main/containers/FileBrowser/index.tsx
@@ -3,28 +3,17 @@ import React from 'react';
 import MarkdownPanel from './MarkdownPanel';
 import stores from '../../stores';
 import PlainTextPanel from './PlainTextPanel';
+import { isMarkdown, isPlaintext } from '/@/utils/uri';
 
-const fileType = (uri: string) => {
-  if (/\.mdx?$/i.test(uri)) {
-    return 'markdown';
-  }
-  if (/^\.[^.]$/.test(uri) || /\.(txt|jsx?|tsx|html)$/.test(uri)) {
-    return 'plaintext';
-  }
-};
+
 
 const ResourcePanel = observer(() => {
   const activatedUri = stores.activationStore.activeFileUri;
 
   return (
     <>
-      <MarkdownPanel
-        visible={fileType(activatedUri) === 'markdown'}
-        uri={activatedUri}
-      />
-      {fileType(activatedUri) === 'plaintext' && (
-        <PlainTextPanel uri={activatedUri} />
-      )}
+      <MarkdownPanel visible={isMarkdown(activatedUri)} uri={activatedUri} />
+      {isPlaintext(activatedUri) && <PlainTextPanel uri={activatedUri} />}
     </>
   );
 });
---
+++ packages/renderer/src/utils/tests/uri.test.ts
@@ -0,0 +1,14 @@
+import { expect, test } from 'vitest';
+import { fileType, isMarkdown } from '../uri';
+
+test('fileType', () => {
+  expect(fileType('file:///abc/abc.md')).toBe('markdown');
+  expect(fileType('file:///abc/.gitignore')).toBe('plaintext');
+  expect(fileType('file:///abc/x.xxxx')).toBe('unknown');
+});
+
+test('isMarkdown', () => {
+  expect(isMarkdown('file:///x/abc.md')).toBe(true);
+  expect(isMarkdown('file:///x/abc.mdx')).toBe(true);
+  expect(isMarkdown('file:///x/abc.ss')).toBe(false);
+});
