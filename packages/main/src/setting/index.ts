2bbb031eaf505fa37ede2ac2ad3d9c887415ac6f
pansinm <pansinm@foxmail.com>
2022-09-27 18:22:50 +0800

    refactor: 实现setting

--- packages/main/src/setting/index.ts
+++ packages/main/src/setting/index.ts
@@ -1,8 +1,55 @@
-import { app } from 'electron';
-import Store from 'electron-store';
+import { app, BrowserWindow, ipcMain } from 'electron';
+import * as fsp from 'fs/promises';
+import * as crypto from 'crypto';
+import { decrypt, encrypt } from '../utils/security';
 
-export default new Store({
-  name: 'onote',
-  cwd: app.getPath('userData'),
-  clearInvalidConfig: true,
+const configFile = app.getPath('userData') + '/.onote-setting.json';
+class Setting {
+  private _setting: Record<string, any> = {};
+  private configFile: string;
+  constructor(configFile: string) {
+    this.configFile = configFile;
+    this.loadFromFile();
+  }
+  private async loadFromFile() {
+    try {
+      const content = await fsp.readFile(this.configFile, 'utf-8');
+      this._setting = JSON.parse(await decrypt(content));
+    } catch (err) {
+      // ignore
+    }
+  }
+  private async syncToFile() {
+    try {
+      const content = await encrypt(JSON.stringify(this._setting));
+      await fsp.writeFile(this.configFile, content);
+    } catch (err) {
+      // ignore
+    }
+  }
+  update(key: string, value: any) {
+    this._setting[key] = value;
+    // 广播，同步到所有窗口
+    BrowserWindow.getAllWindows().forEach((win) =>
+      win.webContents.send('setting.updated', this._setting, key),
+    );
+    this.syncToFile();
+  }
+  getAll() {
+    return this._setting;
+  }
+}
+
+const setting = new Setting(configFile);
+
+// 更新设置
+ipcMain.handle('setting.update', (e, key: string, value: any) => {
+  setting.update(key, value);
 });
+
+// 获取设置
+ipcMain.handle('setting.getAll', async (e) => {
+  return setting.getAll();
+});
+
+export default setting;
