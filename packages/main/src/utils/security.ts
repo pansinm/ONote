7c166a2ea5b75291bd4b3dc6194f84f19e8c0dc5
pansinm <pansinm@foxmail.com>
2022-09-27 18:22:50 +0800

    refactor: 实现setting

---
+++ packages/main/src/utils/security.ts
@@ -0,0 +1,47 @@
+import * as crypto from 'crypto';
+
+//------------------ 加密数据 -------------------
+const algorithm = 'aes-128-cbc'; // algorithm 是算法的意思
+
+export const encrypt = function (text: string): Promise<string> {
+  const password = crypto.randomBytes(16).toString('hex'); // password是用于生产密钥的密码
+  const salt = crypto.randomBytes(16).toString('hex'); // 生成盐值
+  const iv = crypto.randomBytes(8).toString('hex'); // 初始化向量
+
+  return new Promise((resolve, reject) => {
+    crypto.scrypt(password, salt, 16, function (err, derivedKey) {
+      if (err) {
+        reject(err);
+      } else {
+        const cipher = crypto.createCipheriv(algorithm, derivedKey, iv); // 创建 cipher 实例
+
+        // 加密数据
+        let cipherText = cipher.update(text, 'utf8', 'hex');
+        cipherText += cipher.final('hex');
+        cipherText += password + salt + iv;
+
+        resolve(cipherText);
+      }
+    });
+  });
+};
+
+export const decrypt = function (cipherText: string): Promise<string> {
+  const iv = cipherText.slice(-16);
+  const salt = cipherText.slice(-48, -16);
+  const password = cipherText.slice(-80, -48);
+  const data = cipherText.slice(0, -80);
+
+  return new Promise((resolve, reject) => {
+    crypto.scrypt(password, salt, 16, function (err, derivedKey) {
+      if (err) {
+        reject(err);
+      } else {
+        const decipher = crypto.createDecipheriv(algorithm, derivedKey, iv);
+        let txt = decipher.update(data, 'hex', 'utf8');
+        txt += decipher.final('utf8');
+        resolve(txt);
+      }
+    });
+  });
+};
--- packages/renderer/src/rpc/mainServer.ts
+++ packages/renderer/src/rpc/mainServer.ts
@@ -1,6 +1,5 @@
 import type { Engine } from '@hpcc-js/wasm';
 import * as monaco from 'monaco-editor';
-import stores from '../main/stores';
 import Server from './Server';
 
 const server = new Server();
@@ -21,31 +20,6 @@ server.handle('openExternal', async (url: string) => {
   window.simmer.openExternal(url);
 });
 
-server.handle('deleteTodo', async (uri, title) => {
-  const [, note] = /notes\/(.*?)\//.exec(uri) || [];
-  if (note) {
-    const todo = stores.todoStore.todos.find(
-      (todo) => todo?.note?.id === note && todo.title === title,
-    );
-    if (todo) {
-      stores.deleteTodo(todo.id);
-    }
-  }
-});
-
-server.handle('ensureTodo', async (uri, title, status) => {
-  const [, note] = /notes\/(.*?)\//.exec(uri) || [];
-  if (note) {
-    const todo = stores.todoStore.todos.find(
-      (todo) => todo?.note?.id === note && todo.title === title,
-    );
-    if (todo) {
-      stores.updateTodo(todo.id, { status });
-    } else {
-      stores.createTodo({ title, note: { id: note }, status });
-    }
-  }
-});
 
 server.handle(
   'replaceText',
