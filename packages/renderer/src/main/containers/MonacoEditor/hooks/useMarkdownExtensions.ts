6f5af1ab20fae93d5a0959fea811c2556d35ac06
pansinm <pansinm@foxmail.com>
2022-10-26 15:22:51 +0800

    feat: 自动补全支持双引号

--- packages/renderer/src/main/containers/MonacoEditor/hooks/useMarkdownExtensions.ts
+++ packages/renderer/src/main/containers/MonacoEditor/hooks/useMarkdownExtensions.ts
@@ -1,4 +1,4 @@
-import type * as monaco from 'monaco-editor';
+import * as monaco from 'monaco-editor';
 import { useEffect } from 'react';
 import { activate } from '/@/main/monaco';
 import { MonacoMarkdownExtension } from '/@/simmer-markdown/src/ts';
@@ -8,6 +8,33 @@ function useMarkdownExtensions(editor?: monaco.editor.IStandaloneCodeEditor) {
     if (editor) {
       new MonacoMarkdownExtension().activate(editor);
       activate(editor);
+      editor.onKeyDown((e) => {
+        console.log(e);
+        if (e.code === 'Quote' && e.shiftKey) {
+          const selection = editor.getSelection();
+          if (selection) {
+            e.preventDefault();
+            const range = new monaco.Range(
+              selection?.startLineNumber,
+              selection?.startColumn,
+              selection?.endLineNumber,
+              selection?.endColumn,
+            );
+            editor.executeEdits(
+              null,
+              [{ range: range, text: e.shiftKey ? '""' : '\'\'' }],
+              [
+                new monaco.Selection(
+                  range.startLineNumber,
+                  range.startColumn + 1,
+                  range.startLineNumber,
+                  range.startColumn + 1,
+                ),
+              ],
+            );
+          }
+        }
+      });
     }
   }, [editor]);
 }
