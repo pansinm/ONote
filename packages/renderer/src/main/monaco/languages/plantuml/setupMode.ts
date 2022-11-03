ad602baf328fe3aaaa62e9dbb30bf3aef8e66803
pansinm <pansinm@foxmail.com>
2022-11-03 11:03:26 +0800

    refactor: 移动目录

--- packages/renderer/src/main/monaco/languages/plantuml/setupMode.ts
+++ packages/renderer/src/main/monaco/languages/plantuml/setupMode.ts
@@ -1,3 +1,25 @@
+import {
+  CompletionAdapter,
+  DefinitionAdapter,
+  DiagnosticsAdapter,
+  DocumentColorAdapter,
+  DocumentFormattingEditProvider,
+  DocumentHighlightAdapter,
+  DocumentLinkAdapter,
+  DocumentRangeFormattingEditProvider,
+  DocumentSymbolAdapter,
+  FoldingRangeAdapter,
+  HoverAdapter,
+  ReferenceAdapter,
+  RenameAdapter,
+  SelectionRangeAdapter,
+  WorkerManager,
+  fromPosition,
+  fromRange,
+  toRange,
+  toTextEdit,
+} from 'monaco-editor/esm/vs/language/json/jsonMode';
+
 export const setupMode = () => {
   // ignore
 };
