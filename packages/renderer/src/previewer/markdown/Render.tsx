1678cdaa0919d544c17316fad42636d6f94d231e
pansinm <pansinm@foxmail.com>
2022-12-06 09:44:35 +0800

    refactor: move stringify and parse to parser directory

--- packages/renderer/src/previewer/markdown/Render.tsx
+++ packages/renderer/src/previewer/markdown/Render.tsx
@@ -1,9 +1,17 @@
 import type { FC } from 'react';
 import React from 'react';
-import { parse, render } from './utils';
+import { parse } from './parser';
 import usePreviewerScrollSync from '../hooks/usePreviewerScrollSync';
 import { extname, isPlaintext } from '../../common/utils/uri';
 import { Code } from './handlers/code';
+import type { Root } from 'mdast';
+import type { ReactNode } from 'react';
+import createCtx from './createCtx';
+
+function render(fileUri: string, ast: Root, rootDirUri: string): ReactNode {
+  const ctx = createCtx({ fileUri, ast, rootDirUri });
+  return ctx.render(ast, ctx);
+}
 
 interface RenderProps {
   uri: string;
