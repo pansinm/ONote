8234b4f0c358c9e838f3f8607a6da21fd6e5114a
pansinm <pansinm@foxmail.com>
2022-12-13 14:09:33 +0800

    fix: createLineClass return `line-start-undefined` when position is undefined

--- packages/renderer/src/previewer/markdown/handlers/util/position.ts
+++ packages/renderer/src/previewer/markdown/handlers/util/position.ts
@@ -1,5 +1,8 @@
 import type { Root } from 'mdast';
 
 export function createLineClass(position: Root['position']) {
+  if (!position) {
+    return '';
+  }
   return `line-start-${position?.start.line} line-end-${position?.end.line}`;
 }
