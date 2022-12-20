9f10830d66c73f64b2c9da6f7c470c2bc22723e9
pansinm <pansinm@foxmail.com>
2022-12-20 15:43:16 +0800

    chore: unit test config

--- packages/main/jest.config.ts
+++ packages/main/jest.config.ts
@@ -90,11 +90,8 @@ export default {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     ...pathsToModuleNameMapper(compilerOptions.paths, {
-      prefix: '<rootDir>/packages/renderer/',
+      prefix: '<rootDir>/',
     }),
-    'monaco-editor':
-      '<rootDir>/node_modules/monaco-editor/esm/vs/editor/editor.api',
-    '^.+\\.(css|less)$': '<rootDir>/.electron-vendors.cache.json',
   },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
