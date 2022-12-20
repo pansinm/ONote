b5cc5c040050ed9d4bad4989f35246e099ab4961
pansinm <pansinm@foxmail.com>
2022-12-20 15:58:23 +0800

    tests: add emoji parse test

--- packages/renderer/src/previewer/markdown/parser/tests/index.test.ts
+++ packages/renderer/src/previewer/markdown/parser/tests/index.test.ts
@@ -1,5 +1,14 @@
-import { stringify } from '..';
+import { parse, stringify } from '..';
 
 test('stringify emoji', () => {
   expect(stringify({ type: 'emoji', name: 'hash' } as any)).toBe(':hash:');
+});
+
+test('parse emoji', () => {
+  expect(parse(':hash:')).toMatchObject({
+    type: 'root',
+    children: [
+      { type: 'paragraph', children: [{ type: 'emoji', name: 'hash' }] },
+    ],
+  });
 });
