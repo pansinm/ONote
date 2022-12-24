75ce8306d9aab139c24e1d1677e9304f98772b66
pansinm <pansinm@foxmail.com>
2022-12-24 18:35:17 +0800

    refact: add factory

--- packages/main/src/tunnel/index.ts
+++ packages/main/src/tunnel/index.ts
@@ -9,3 +9,28 @@ ipcMain.handle('request-port', async (ev) => {
   mainWindow?.webContents.postMessage('port', null, [port1]);
   ev.sender.postMessage('port', null, [port2]);
 });
+
+ipcMain.handle('create-tunnel-port', async (ev, payload, meta) => {
+  const { port1, port2 } = new MessageChannelMain();
+  const mainWindow = await BrowserWindow.getAllWindows().find(
+    (w) => w.webContents.getURL() === getPageUrl('main'),
+  );
+  mainWindow?.webContents.postMessage(
+    'channel-port',
+    {
+      channel: 'channel-port',
+      payload,
+      meta,
+    },
+    [port1],
+  );
+  ev.sender.postMessage(
+    'channel-port',
+    {
+      channel: 'channel-port',
+      payload,
+      meta,
+    },
+    [port2],
+  );
+});
