f3a6bb27cecdad49ae2ba19f5e8656f6290c6a57
pansinm <pansinm@foxmail.com>
2023-01-03 15:53:17 +0800

    fix: yarn jest --coverage报错,暂时屏蔽

--- packages/renderer/setup/index.ts
+++ packages/renderer/setup/index.ts
@@ -1,6 +1,11 @@
 // todo: move to setup
 window.MessageChannel = require('worker_threads').MessageChannel;
 
+const _postMessage = (window as any)._postMessage || window.postMessage;
+if (!(window as any)._postMessage) {
+  (window as any)._postMessage = _postMessage.bind(window);
+}
+
 function postMessage(message, targetOrigin, transfer) {
   const event = window.document.createEvent('messageevent') as MessageEvent;
   event.initEvent('message');
@@ -13,9 +18,12 @@ function postMessage(message, targetOrigin, transfer) {
   const e = event as any;
   e._type = 'message';
   e.data = message;
-  e.ports = transfer;
-
-  this.dispatchEvent(event);
+  if (transfer) {
+    e.ports = transfer;
+    window.dispatchEvent(event);
+  } else {
+    (window as any)._postMessage(message, targetOrigin);
+  }
 }
 
 window.postMessage = postMessage.bind(window);
