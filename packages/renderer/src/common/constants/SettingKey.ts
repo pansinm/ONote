fb0524233638221f6af4e19cd574248c5ba312b5
pansinm <pansinm@foxmail.com>
2023-04-24 15:04:50 +0800

    feat: 编辑器支持换行

--- packages/renderer/src/common/constants/SettingKey.ts
+++ packages/renderer/src/common/constants/SettingKey.ts
@@ -1,5 +1,7 @@
 export const EDITOR_MODE = 'editor.mode';
 
+export const EDITOR_WORD_WRAP = 'editor.word-wrap';
+
 export const EDITOR_FONT_FAMILY = 'editor.font-family';
 
 export const PLANTUML_ENDPOINT = 'plantuml.endpoint';
