2d7178ddb79fc978ab2e28d21769fba4f0253161
pansinm <pansinm@foxmail.com>
2023-11-23 11:10:56 +0800

    feat: inline color 支持显示颜色

--- packages/renderer/src/previewer/markdown/handlers/inline-code.tsx
+++ packages/renderer/src/previewer/markdown/handlers/inline-code.tsx
@@ -2,6 +2,33 @@ import React from 'react';
 import type { InlineCode } from 'mdast';
 import { createLineClass } from './util/position';
 
+function isColor(text: string) {
+  return /^(#[0-9A-F]{3}|#[0-9A-F]{6}|rgba?\([ ,0-9]+?\)|hsl\([ 0-9%]\))$/i.test(
+    text,
+  );
+}
+
+function ColorDot({ color }: { color: string }) {
+  return (
+    <span
+      style={{
+        display: 'inline-block',
+        background: color,
+        border: 0,
+        borderRadius: 10,
+        width: 10,
+        height: 10,
+        marginLeft: 5,
+      }}
+    ></span>
+  );
+}
+
 export default function inlineCode(node: InlineCode, ctx: any) {
-  return <code className={createLineClass(node.position)}>{node.value}</code>;
+  return (
+    <code className={createLineClass(node.position)}>
+      {node.value}
+      {isColor(node.value) ? <ColorDot color={node.value} /> : null}
+    </code>
+  );
 }
