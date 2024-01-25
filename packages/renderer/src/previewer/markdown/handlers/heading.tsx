348032a181ed447e0cba4e5c578381a148cf6028
pansinm <pansinm@foxmail.com>
2024-01-25 16:53:18 +0800

    fix: heading包含连接时,TOC渲染不正确

--- packages/renderer/src/previewer/markdown/handlers/heading.tsx
+++ packages/renderer/src/previewer/markdown/handlers/heading.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import type { Heading } from 'mdast';
-import { stringify } from '../../../common/markdown';
+import { getText } from '../../../common/markdown';
 import { createLineClass } from './util/position';
 
 export default function heading(node: Heading, ctx: any) {
   // eslint-disable-next-line react/no-children-prop
   return React.createElement('h' + node.depth, {
     // ### abc  => abc
-    id: encodeURIComponent(
-      stringify(node)
-        .trim()
-        .slice(node.depth + 1),
-    ),
+    id: encodeURIComponent(getText(node)),
     className: createLineClass(node.position),
     children: ctx.renderChildren(node, ctx),
   });
