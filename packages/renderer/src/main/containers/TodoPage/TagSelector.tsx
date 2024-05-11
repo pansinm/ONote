334d660f5b380e392abdf66df22fcb740786b825
pansinm <pansinm@foxmail.com>
2024-05-11 18:13:13 +0800

    feat: 将标签新增按钮放到右侧面板

--- packages/renderer/src/main/containers/TodoPage/TagSelector.tsx
+++ packages/renderer/src/main/containers/TodoPage/TagSelector.tsx
@@ -12,6 +12,7 @@ import {
 } from '@fluentui/react-tag-picker-preview';
 import { createTagColorStyle } from '/@/common/utils/style';
 import { observer } from 'mobx-react-lite';
+import TagCreateForm from './TagCreateForm';
 
 interface TagSelectorProps {
   onChange(tags: string[]): void;
@@ -22,48 +23,52 @@ const TagSelector = function ({ onChange, tags }: TagSelectorProps) {
   const tagPickerOptions = stores.todoStore.tags.map((tag) => tag.name);
   const selectedOptions: string[] = tags;
   return (
-    <TagPicker
-      size="medium"
-      onOptionSelect={(_, { selectedOptions }) => {
-        onChange(selectedOptions);
-      }}
-      selectedOptions={selectedOptions}
-    >
-      <TagPickerControl>
-        <TagPickerGroup>
-          {selectedOptions.map((option) => (
-            <Tag
-              key={option}
-              shape="rounded"
-              value={option}
-              style={createTagColorStyle(
-                option,
-                stores.todoStore.tagRecords[option]?.color,
-              )}
-            >
-              {option}
-            </Tag>
-          ))}
-        </TagPickerGroup>
-        <TagPickerInput placeholder="选择标签" />
-      </TagPickerControl>
-      <TagPickerList>
-        {tagPickerOptions.length > 0
-          ? tagPickerOptions.map((option) => (
-              <TagPickerOption
-                value={option}
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <TagPicker
+        size="medium"
+        onOptionSelect={(_, { selectedOptions }) => {
+          onChange(selectedOptions);
+        }}
+        selectedOptions={selectedOptions}
+      >
+        <TagPickerControl>
+          <TagPickerGroup>
+            {selectedOptions.map((option) => (
+              <Tag
                 key={option}
+                shape="rounded"
+                value={option}
                 style={createTagColorStyle(
                   option,
                   stores.todoStore.tagRecords[option]?.color,
                 )}
               >
                 {option}
-              </TagPickerOption>
-            ))
-          : 'No options available'}
-      </TagPickerList>
-    </TagPicker>
+              </Tag>
+            ))}
+          </TagPickerGroup>
+          <TagPickerInput placeholder="选择标签" />
+        </TagPickerControl>
+
+        <TagPickerList>
+          {tagPickerOptions.length > 0
+            ? tagPickerOptions.map((option) => (
+                <TagPickerOption
+                  value={option}
+                  key={option}
+                  style={createTagColorStyle(
+                    option,
+                    stores.todoStore.tagRecords[option]?.color,
+                  )}
+                >
+                  {option}
+                </TagPickerOption>
+              ))
+            : 'No options available'}
+        </TagPickerList>
+      </TagPicker>
+      <TagCreateForm />
+    </div>
   );
 };
 
