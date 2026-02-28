03aa08f3b6fe1490895af9dfffc2e3fc846883dd
pansinm <pansinm@foxmail.com>
2026-02-28 09:06:25 +0800

    feat: feat: 优化预览器字体并修复样式加载

--- packages/renderer/src/previewer/index.tsx
+++ packages/renderer/src/previewer/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.scss';
 import App from './components/App';
 import '/@/common/emoji/emoji.scss';
 import './integration';
 import { getLogger } from '/@/shared/logger';
+import './index.scss';
 
 const logger = getLogger('Previewer');
 
@@ -14,4 +14,6 @@ window.React = React;
 const root = createRoot(document.getElementById('app') as HTMLDivElement);
 root.render(<App />);
 
-window.addEventListener('message', (e) => logger.debug('Window message', e.data));
+window.addEventListener('message', (e) =>
+  logger.debug('Window message', e.data),
+);
